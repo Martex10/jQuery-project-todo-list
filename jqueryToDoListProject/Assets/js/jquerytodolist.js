d79cddc3ed40a7dde2d3f943946d447f77217432
Marcus Hobdy <hobdy.marcusjr@gmail.com>
2018-03-21 23:29:20 -0400

    first draft complete
    first draft is complete

--- jqueryToDoListProject/Assets/js/jquerytodolist.js
+++ jqueryToDoListProject/Assets/js/jquerytodolist.js
@@ -4,7 +4,7 @@ $("ul").on("click", "li", function(){
 });
 //click on x to delete todo
 $("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(1000, function(){
+    $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
@@ -16,6 +16,9 @@ $("input[type='text']").keypress(function(event){
         let todoText = $(this).val();
         $(this).val("");
         //create a new li and add to ul
-        $("ul").append("<li>" + "<span>X</span> " + todoText + "</li>");
+        $("ul").append("<li>" + '<span><i class="fas fa-trash"></i></span> ' + todoText + "</li>");
     }
+});
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle()
 });
